8c3f25b9938c21e7508ea405a1272e4db92ab0d3
Bin Yu <binyu.fi@gmail.com>
2017-02-28 18:23:58 +0200

    Add Maximum Subarray Size Sum To K.

---
+++ phone-screen-practice/PhoneScreenPractice/PhoneScreenPractice/MaximumSubarraySizeSumToK.h
@@ -0,0 +1,15 @@
+//
+//  MaximumSubarraySizeSumToK.h
+//  PhoneScreenPractice
+//
+//  Created by Bin Yu on 28/02/2017.
+//  Copyright © 2017 Bin Yu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MaximumSubarraySizeSumToK : NSObject
+
+- (NSUInteger)maximumSizeSubarrayInArray:(NSArray<NSNumber *> *)nums k:(NSInteger)k;
+
+@end
---
+++ phone-screen-practice/PhoneScreenPractice/PhoneScreenPractice/MaximumSubarraySizeSumToK.m
@@ -0,0 +1,26 @@
+//
+//  MaximumSubarraySizeSumToK.m
+//  PhoneScreenPractice
+//
+//  Created by Bin Yu on 28/02/2017.
+//  Copyright © 2017 Bin Yu. All rights reserved.
+//
+
+#import "MaximumSubarraySizeSumToK.h"
+
+@implementation MaximumSubarraySizeSumToK
+
+- (NSUInteger)maximumSizeSubarrayInArray:(NSArray<NSNumber *> *)nums k:(NSInteger)k {
+    NSMutableDictionary<NSNumber *, NSNumber *> *preSums = [NSMutableDictionary dictionary];
+    NSUInteger maxSize = 0;
+    NSInteger sum = 0;
+    for (NSInteger i = 0; i<nums.count; i++) {
+        sum += nums[i].integerValue;
+        if (sum == k) maxSize = MAX(maxSize, i+1);
+        if (preSums[@(sum-k)]) maxSize = MAX(maxSize, i-preSums[@(sum-k)].integerValue);
+        if (preSums[@(sum)]) preSums[@(sum)] = @(i);
+    }
+    return maxSize;
+}
+
+@end
