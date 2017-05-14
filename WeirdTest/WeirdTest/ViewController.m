c1d65497b90358e682ab6352ca705b330d4f2909
Bin Yu <binyu.fi@gmail.com>
2017-05-15 00:42:46 +0300

    Some changes.

--- WeirdTest/WeirdTest/ViewController.m
+++ WeirdTest/WeirdTest/ViewController.m
@@ -20,7 +20,29 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [self summaryRanges];
+    [self regularExpressionMatching];
+}
+
+- (void)regularExpressionMatching {
+    NSString *s = @"aa";
+    NSString *p = @"a";
+    NSLog(@"%@ matches %@: %@", s, p, BOOLSTR([self isMatch:s p:p]));
+    
+    s = @"aa";
+    p = @"a*";
+    NSLog(@"%@ matches %@: %@", s, p, BOOLSTR([self isMatch:s p:p]));
+    
+    s = @"aa";
+    p = @".*";
+    NSLog(@"%@ matches %@: %@", s, p, BOOLSTR([self isMatch:s p:p]));
+    
+    s = @"ab";
+    p = @".*";
+    NSLog(@"%@ matches %@: %@", s, p, BOOLSTR([self isMatch:s p:p]));
+    
+    s = @"aab";
+    p = @"c*a*b";
+    NSLog(@"%@ matches %@: %@", s, p, BOOLSTR([self isMatch:s p:p]));
 }
 
 - (void)summaryRanges {
@@ -354,6 +376,40 @@
 }
 
 #pragma mark - Private
+
+#pragma mark - Regular Expression Matching
+
+- (BOOL)isMatch:(NSString *)s p:(NSString *)p {
+    if (s == nil || p == nil) return NO;
+    BOOL dp[s.length+1][p.length+1];
+    // initialize the dp container
+    // empty string matches empty string
+    dp[0][0] = YES;
+    // if the length of p is 0, then we would say s is not match to empty string
+    for (NSInteger i=1; i<=s.length; i++) dp[i][0] = NO;
+    // if the length of s is 0, then if the previous character in p p[j-1] == '*'
+    // and also previous character is NOT the first one, then we could say dp[0][j] = dp[0][j-2]
+    for (NSInteger j=1; j<=p.length; j++) {
+        unichar c = [p characterAtIndex:j-1];
+        dp[0][j] = j > 1 && c == '*' && dp[0][j-2];
+    }
+    
+    // now we can build the DP table
+    for (NSInteger i=1; i<=s.length; i++) {
+        for (NSInteger j=1; j<=p.length; j++) {
+            unichar sc = [s characterAtIndex:i-1];
+            unichar pc = [p characterAtIndex:j-1];
+            if (pc != '*') {
+                dp[i][j] = dp[i-1][j-1] && (sc == pc || pc == '.');
+            } else if (j > 1) {
+                unichar pcc = [p characterAtIndex:j-2];
+                dp[i][j] = dp[i][j-2] || ((sc == pcc || pcc == '.') && dp[i-1][j]);
+            }
+        }
+    }
+    return dp[s.length][p.length];
+}
+
 
 #pragma mark - Expression Add Operators
 
